cdfab43e6df0d183aabe6ff2e2ed972984e03d83
Maël Kervella <dev@maelkervella.eu>
2017-12-13 17:21:25 +0100

    QML Init: Model for an item of the playlist

---
+++ modules/gui/qt/components/playlist/plitem.hpp
@@ -0,0 +1,32 @@
+#ifndef PLITEM_HPP
+#define PLITEM_HPP
+
+#include "qt.hpp"
+#include <qt5/QtCore/QString>
+#include "components/mediacenter/mlalbumtrack.hpp"
+
+
+class PLItem
+{
+public:
+    PLItem( MLAlbumTrack* _item);
+    QString getTitle();
+    QString getAlbumTitle();
+    QString getName();
+    QString getDuration();
+    QString getCover();
+
+    input_item_t* getInputItem() { return inputItem; }
+
+    void activate( playlist_t* pl );
+
+private:
+    MLAlbumTrack* getItem() { return pl_item; }
+
+private:
+    MLAlbumTrack* pl_item;
+
+    input_item_t* inputItem;
+};
+
+#endif // PLITEM_HPP
